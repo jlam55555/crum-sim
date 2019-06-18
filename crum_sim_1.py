a3b253a7a57752a15f84a252de3d262df533a637
Jonathan Lam <jlam55555@gmail.com>
2019-06-18 01:41:13 -0400

    added basic control button functionality

--- crum_sim_1.py
+++ crum_sim_1.py
@@ -1,16 +1,28 @@
-# math imports
 import numpy as np
+import os
 from math import sqrt
-
-# necessary for showing figures in separate window
-import matplotlib
-matplotlib.use('Qt5Agg')
+from time import sleep
+from threading import Thread
 
 # plotting imports
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 
-plt.figure('Traffic Algorithm Visualization')
+# mockup crum
+class CRUMInterface:
+    iter = 0
+    def advance(self):
+        self.iter += 1
+        print('Advanced to iteration %i' % self.iter)
+    def reset(self):
+        self.iter = 0
+crumIntf = CRUMInterface()
+
+# necessary for showing figures in separate window
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+simFigure = plt.figure('Traffic Algorithm Visualization')
 
 # draw Braess's network
 plt.subplot(1, 2, 1)
@@ -44,25 +56,47 @@ gParamSlid = Slider(gParamAxis, 'G', 0, 1, valinit=0.5)
 pParamSlid = Slider(pParamAxis, 'p', 0, 1, valinit=0.5)
 
 # controlling the simulation
+isPlaying = False
 resetActionAxis = plt.axes([0.525, 0.900, 0.075, 0.03])
 nextActionAxis = plt.axes([0.650, 0.900, 0.075, 0.03])
 playActionAxis = plt.axes([0.775, 0.900, 0.200, 0.03])
 
-resetActionButton = Button(resetActionAxis, 'Next Step')
-nextActionButton = Button(nextActionAxis, 'Reset Sim')
+resetActionButton = Button(resetActionAxis, 'Reset sim')
+nextActionButton = Button(nextActionAxis, 'Next step')
 playActionButton = Button(playActionAxis, 'Play/Pause')
 
-def resetActionHandler():
-    pass;
+def resetActionHandler(val):
+    global isPlaying
+    isPlaying = False
+    crumIntf.reset()
+    print('Simulation reset')
 resetActionButton.on_clicked(resetActionHandler)
 
-def nextActionHandler():
-    pass;
+def nextActionHandler(val):
+    if not isPlaying:
+        crumIntf.advance()
 nextActionButton.on_clicked(nextActionHandler)
 
-def playActionHandler():
-    pass;
-resetActionButton.on_clicked(playActionHandler)
+# loop indefinitely in another thread
+def loopCrum():
+    while 1:
+        if isPlaying:
+            crumIntf.advance()
+        sleep(1)
+loopCrumThread = Thread(target=loopCrum, daemon=True)
+loopCrumThread.start()
+
+def playActionHandler(val):
+    global isPlaying
+    isPlaying = not isPlaying
+    print('Changed isPlaying: %r' % isPlaying)
+playActionButton.on_clicked(playActionHandler)
 
 # show figure
 plt.show()
+
+# cleanly close all threads when window closed
+def closeActionHandler(val):
+    print('Closing simulation')
+    os._exit(0)
+simFigure.canvas.mpl_connect('close_event', closeActionHandler)
