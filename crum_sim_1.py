a7de8664d4624fe8c59af0331635a156996aee65
Jonathan Lam <jlam55555@gmail.com>
2019-06-20 03:08:28 -0400

    added chart and network visualizations

--- crum_sim_1.py
+++ crum_sim_1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-from random import randint
+from random import randint, random
 from math import sqrt
 from time import sleep
 from threading import Thread
@@ -11,30 +11,33 @@ from matplotlib.widgets import Slider, Button
 
 # mockup crum
 class MockCRUMInterface(object):
-    def __init__(self, n):
+    def __init__(self, n, G, p):
         self.iter = 0
         self.n = n
+        self.G = G
+        self.p = p
         self.vehicles = [randint(0, 2) for x in range(self.n)]
+    # these methods aren't too efficient right now, called more than once in a cycle
     def travelTimeEdges(self):
         routeCounts = self.countRoutes()
-        travelTimeEdgesMap = {
-            0: (routeCounts[0] + routeCounts[1]) / 100,
-            1: 45,
-            2: (routeCounts[1] + routeCounts[2]) / 100,
-            3: 45,
-            4: 0
-        }
+        travelTimeEdgesMap = [
+            (routeCounts[0] + routeCounts[1]) / 100.0,
+            45,
+            45,
+            (routeCounts[1] + routeCounts[2]) / 100.0,
+            0
+        ]
         return travelTimeEdgesMap
     def travelTimeRoutes(self):
         edgeTimes = self.travelTimeEdges()
-        travelTimeMap = {
-            0: edgeTimes[0] + edgeTimes[1],
-            1: edgeTimes[0] + edgeTimes[3],
-            2: edgeTimes[2] + edgeTimes[3]
-        }
+        travelTimeMap = [
+            edgeTimes[0] + edgeTimes[1],
+            edgeTimes[0] + edgeTimes[3],
+            edgeTimes[2] + edgeTimes[3]
+        ]
         return travelTimeMap
     def countRoutes(self):
-        countMap = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0 }
+        countMap = [ 0, 0, 0 ]
         for v in self.vehicles:
             countMap[v] += 1
         return countMap
@@ -42,7 +45,13 @@ class MockCRUMInterface(object):
         self.iter += 1
         setIterParamText(self.iter)
         print('Advanced to iteration %i' % self.iter)
-        self.vehicles = [randint(0, 2) for x in range(self.n)]
+        # self.vehicles = [randint(0, 2) for x in range(self.n)]
+        for i in range(self.n):
+            if(random() > self.G):
+                if(random() > self.p):
+                    self.vehicles[i] = randint(0, 2)
+                else:
+                    self.vehicles[i] = [ 0, 2 ][randint(0, 1)]
         return {
             'iter': self.iter,
             'routeCounts': self.countRoutes(),
@@ -52,7 +61,13 @@ class MockCRUMInterface(object):
     def reset(self):
         self.iter = 0
         setIterParamText(self.iter)
-crumIntf = MockCRUMInterface(50)
+crumIntf = MockCRUMInterface(4000, 0.5, 0.5)
+crumData = {
+    'iter': [],
+    'routeCounts': [ [], [], [] ],
+    'edgeTimes': [ [], [], [], [], [] ],
+    'routeTimes': [ [], [], [] ],
+}
 print(crumIntf)
 
 # necessary for showing figures in separate window
@@ -62,7 +77,7 @@ matplotlib.use('Qt5Agg')
 simFigure = plt.figure('CRUM Traffic Routing Algorithm')
 
 # draw Braess's network
-plt.subplot(1, 2, 1)
+networkPlot = plt.subplot(1, 2, 1)
 
 # let bx (Braess x-pos) be x-position of left and right points (for convenience)
 bx = sqrt(3)/2
@@ -77,7 +92,7 @@ edgeCoords = [
     [[bx, 0], [0, -0.5]],
     [[0, 0], [-0.5, 0.5]]
 ]
-edges = [plt.plot(edgeCoords[i][0], edgeCoords[i][1], c=colors[i])[0] for i in range(5)]
+edges = [plt.plot(edgeCoords[i][0], edgeCoords[i][1], c=colors[i], solid_capstyle='round')[0] for i in range(5)]
 
 plt.scatter([-bx, 0, 0, bx], [0, 0.5, -0.5, 0], c='grey')
 
@@ -91,6 +106,13 @@ pParamAxis = plt.axes([0.775, 0.950, 0.200, 0.03])
 
 gParamSlid = Slider(gParamAxis, 'G', 0, 1, valinit=0.5)
 pParamSlid = Slider(pParamAxis, 'p', 0, 1, valinit=0.5)
+
+def gParamHandler(val):
+    crumIntf.G = val
+def pParamHandler(val):
+    crumIntf.p = val
+gParamSlid.on_changed(gParamHandler)
+pParamSlid.on_changed(pParamHandler)
 
 # controlling the simulation
 isPlaying = False
@@ -145,26 +167,67 @@ isPlayingCtrlAxis.axis('off')
 iterParamAxis.axis('off')
 
 # showing charts
-# % improvement of car flow vs. rounds
+# car route times
 plt.subplot(4, 2, 4)
-plt.title('Improvement of Car Flow')
-plt.plot([], [])
+plt.title('Car Route Times')
+carRouteTimePlot = plt.plot([], [], 'r-', [], [], 'b-', [], [], 'g-')
 
-# % improvement of travel time vs. rounds
+# edge travel times
 plt.subplot(4, 2, 6)
-plt.title('Improvement of Travel Time')
-plt.plot([], [])
+plt.title('Edge Travel Times')
+carEdgeTimePlot = plt.plot([], [], colors[0], [], [], colors[1], [], [], colors[2], [], [], colors[3], [], [], colors[4])
 
 # % car flow distribution vs. rounds
 plt.subplot(4, 2, 8)
 plt.title('Car Flow')
-plt.plot([], [])
+carFlowPlot = plt.plot([], [], 'r-', [], [], 'b-', [], [], 'g-')
 
 # advance
 def getNextStep():
-    stepData = crumIntf.advance()
+    crumStepData = crumIntf.advance()
+    crumData['iter'].append(crumStepData['iter'])
 
-    print(stepData)
+    print(crumStepData)
+
+    for route, count in enumerate(crumStepData['routeCounts']):
+        crumData['routeCounts'][route].append(count)
+        carFlowPlot[route].set_xdata(crumData['iter'])
+        carFlowPlot[route].set_ydata(crumData['routeCounts'][route])
+
+    for edge, time in enumerate(crumStepData['edgeTimes']):
+        crumData['edgeTimes'][edge].append(time)
+        carEdgeTimePlot[edge].set_xdata(crumData['iter'])
+        carEdgeTimePlot[edge].set_ydata(crumData['edgeTimes'][edge])
+
+    for route, time in enumerate(crumStepData['routeTimes']):
+        crumData['routeTimes'][route].append(time)
+        carRouteTimePlot[route].set_xdata(crumData['iter'])
+        carRouteTimePlot[route].set_ydata(crumData['routeTimes'][route])
+
+    edges[0].set_linewidth((crumStepData['routeCounts'][0] + crumStepData['routeCounts'][2]) / 100)
+    edges[1].set_linewidth(crumStepData['routeCounts'][0] / 100)
+    edges[2].set_linewidth(crumStepData['routeCounts'][2] / 100)
+    edges[3].set_linewidth((crumStepData['routeCounts'][1] + crumStepData['routeCounts'][2]) / 100)
+    edges[4].set_linewidth(crumStepData['routeCounts'][1] / 100)
+
+    edgeLabels = []
+    for ind, edge in enumerate(edges):
+        edgeLabels.append("edge %i; count %i; latency %i" % (ind, edge.get_linewidth() * 100, crumStepData['edgeTimes'][ind]))
+    networkPlot.legend(edges, edgeLabels)
+
+    ax = carFlowPlot[0].axes
+    ax.relim()
+    ax.autoscale_view()
+
+    ax = carEdgeTimePlot[0].axes
+    ax.relim()
+    ax.autoscale_view()
+
+    ax = carRouteTimePlot[0].axes
+    ax.relim()
+    ax.autoscale_view()
+
+    plt.draw()
 
 # show figure
 simFigure.tight_layout()
